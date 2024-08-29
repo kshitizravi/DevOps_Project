9fa50c0b44473979308e3c1839b0a9b87b93f51f
Kshitiz Ravi <kshitiz.ravi@gmail.com>
2024-08-29 16:54:06 +0530

    Made changes to build.sh and deploy.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Build Docker image
-sudo docker build -t ravikshitiz/dev:latest .
+docker build -t ravikshitiz/dev:latest .
 
 # Tag the image for the dev repository
-sudo docker tag react-app:latest ravikshitiz/dev:latest
+docker tag react-app:latest ravikshitiz/dev:latest
 
 # Push to Docker Hub
-sudo docker push ravikshitiz/dev-app:latest
+docker push ravikshitiz/dev-app:latest
--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Pull the latest image
-sudo docker pull ravikshitiz/dev:latest
+docker pull ravikshitiz/dev:latest
 
 # Stop the currently running container
-sudo docker stop react-app || true
-sudo docker rm react-app || true
+docker stop react-app || true
+docker rm react-app || true
 
 # Run a new container with the latest image
-sudo docker run -idt --name react-app -p 80:80 ravikshitiz/dev:latest
+docker run -idt --name react-app -p 80:80 ravikshitiz/dev:latest
