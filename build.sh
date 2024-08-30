4edb2379ed6f4abe4cb4dfdfc0ff8ce6d198ddcb
Kshitiz Ravi <kshitiz.ravi@gmail.com>
2024-08-30 11:28:16 +0530

    Made changes to build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 docker build -t react-app:latest .
 
 # Tag the image for the dev repository
-docker tag react-app:latest ravikshitiz/dev:latest
+docker tag react-app:latest ravikshitiz/dev-app:latest
 
 # Push to Docker Hub
 docker push ravikshitiz/dev-app:latest
