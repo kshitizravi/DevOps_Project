9e79df90cde88d3f21d28fbc35384bd37eb47e95
Kshitiz Ravi <kshitiz.ravi@gmail.com>
2024-08-31 13:13:05 +0530

    Added prometheus support

--- deploy.sh
+++ deploy.sh
@@ -9,3 +9,13 @@ docker rm react-app || true
 
 # Run a new container with the latest image
 docker run -idt --name react-app -p 80:80 ravikshitiz/dev:latest
+
+
+docker pull ubuntu/prometheus
+
+docker run -d \
+  --name prometheus \
+  -p 9090:9090 \
+  -v /ubuntu/home/prometheus.yml:/etc/prometheus/prometheus.yml \
+  -v /ubuntu/home/prometheus/data:/prometheus \
+  ubuntu/prometheus
