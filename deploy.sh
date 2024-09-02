b6846ea22c5c3fb91ee5cffbc119c956907f4ce1
Kshitiz Ravi <kshitiz.ravi@gmail.com>
2024-09-02 10:34:38 +0530

    Final Commit

--- deploy.sh
+++ deploy.sh
@@ -10,14 +10,3 @@ docker rm react-app || true
 # Run a new container with the latest image
 docker run -idt --name react-app -p 80:80 ravikshitiz/dev:latest
 
-#Pull prometheus latest image
-docker pull ubuntu/prometheus
-
-#Run prometheus inside docker
-docker run -d \
-  --name prometheus \
-  -p 9090:9090 \
-  -v /home/ubuntu/prometheus.yml:/etc/prometheus/prometheus.yml \
-  -v /home/ubuntu/prometheus/data:/prometheus \
-  ubuntu/prometheus
-
